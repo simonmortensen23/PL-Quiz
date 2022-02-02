e801b6c4223b24640d11f39dde8ea58397323f8f
simonmortensen23 <simon020393@gmail.com>
2022-02-02 14:29:33 +0000

    Add show / remove to howToPlay function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 const play = document.querySelector('#special-form');
 
 play.addEventListener('submit', event => {
+    
   event.preventDefault();
   var user = document.getElementById('user');
   if (user.value.length >= 3){
-  
-     window.location.assign("/game.html") 
+    window.location.assign("/game.html") 
   } else {
       alert("Username is too short");
   }
-  console.log(user);
+  
 });
 
 
@@ -19,7 +19,12 @@ play.addEventListener('submit', event => {
  */
 function howToPlay() {
     let htp = document.getElementById('htpText')
+    if (htp.style.width == "0px"){
     htp.innerHTML = '<h2>How To Play</h2> <br> Rules:'
-    htp.style.width = "50%"
+    htp.style.width = "200px"
     htp.style.backgroundColor = "purple"
+} else {
+    htp.style.width = "0px"
+    htp.innerHTML = ""
+    }
 }
