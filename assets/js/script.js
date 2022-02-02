b48f8b85098a545f50e2f9a4013e9fb4b4584ad2
simonmortensen23 <simon020393@gmail.com>
2022-02-02 13:01:46 +0000

    Add howToPlay() function

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function howToPlay() {
+    let htp = document.getElementById('htpText')
+    htp.innerHTML = '<h2>How To Play</h2> <br> Rules:'
+    htp.style.width = "50%"
+    htp.style.backgroundColor = "purple"
+}
