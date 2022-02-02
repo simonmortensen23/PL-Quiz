af0c9eabd6489c22d0e807377d2c1d19414e512d
simonmortensen23 <simon020393@gmail.com>
2022-02-02 13:32:12 +0000

    Add submit username eventListener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+const play = document.querySelector('#special-form');
+
+play.addEventListener('submit', event => {
+  event.preventDefault();
+  var user = document.getElementById('user');
+  if (user.value.length >= 3){
+  
+     window.location.assign("/game.html") 
+  } else {
+      alert("Username is too short");
+  }
+  console.log(user);
+});
+
+
+
+/**
+ * Function that reveals How To Play section in bottom of page
+ */
 function howToPlay() {
     let htp = document.getElementById('htpText')
     htp.innerHTML = '<h2>How To Play</h2> <br> Rules:'
