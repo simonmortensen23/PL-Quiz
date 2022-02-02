6f3767a2f3fe0cefc7b582f5c9c453deaba3df00
simonmortensen23 <simon020393@gmail.com>
2022-02-02 15:06:21 +0000

    Changed logic in howToPlay()

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,8 @@ play.addEventListener('submit', event => {
   event.preventDefault();
   var user = document.getElementById('user');
   if (user.value.length >= 3){
-    window.location.assign("/game.html") 
+    window.location.assign("/game.html")
+   
   } else {
       alert("Username is too short");
   }
@@ -19,12 +20,11 @@ play.addEventListener('submit', event => {
  */
 function howToPlay() {
     let htp = document.getElementById('htpText')
-    if (htp.style.border == "0px"){
+    if (htp.innerHTML == ""){
     htp.innerHTML = "<h2>How To Play</h2> <br> Rules: <ul><li>Enter Username</li><li>The player gets 10 questions</li><li>1 point per question</li><li>Three wrong answers and the game is over</li></ul>"
-    htp.style.width = "50%"
+    htp.style.width = "50vw"
     htp.style.border = "4px solid #37003c"
 } else {
-   
     htp.innerHTML = ""
     htp.style.border = "0px"
     }
