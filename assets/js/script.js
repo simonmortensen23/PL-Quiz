d42693a9f8ee82664614dbe755c4a80d926981bb
simonmortensen23 <simon020393@gmail.com>
2022-02-02 15:11:02 +0000

    Add box-shadow to htp box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
 const play = document.querySelector('#special-form');
 
 play.addEventListener('submit', event => {
-    
   event.preventDefault();
   var user = document.getElementById('user');
   if (user.value.length >= 3){
     window.location.assign("/game.html")
-   
   } else {
       alert("Username is too short");
   }
@@ -19,13 +17,16 @@ play.addEventListener('submit', event => {
  * Function that reveals How To Play section in bottom of page
  */
 function howToPlay() {
+    
     let htp = document.getElementById('htpText')
     if (htp.innerHTML == ""){
     htp.innerHTML = "<h2>How To Play</h2> <br> Rules: <ul><li>Enter Username</li><li>The player gets 10 questions</li><li>1 point per question</li><li>Three wrong answers and the game is over</li></ul>"
     htp.style.width = "50vw"
     htp.style.border = "4px solid #37003c"
+    htp.style.boxShadow = "5px 10px #37003c"
 } else {
     htp.innerHTML = ""
     htp.style.border = "0px"
     }
+    
 }
